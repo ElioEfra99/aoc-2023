19b4f7c2767a7d2d2aaa2561599d83f395ed9326
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2023-12-02 13:31:42 -0600

    Day 1, Part 2 draft

--- day1/part2.swift
+++ day1/part2.swift
@@ -15,28 +15,23 @@ do {
 let calibrationValues = fileString.split(separator: "\n")
 var calibrationValueSum = 0
 
-for value in calibrationValues {
-    let array = Array(value.split(separator: ""))
+for row in calibrationValues {
+    let array = Array(row.split(separator: ""))
     
-    var ptr1 = 0
-    var ptr2 = array.count - 1
+    // Convert the string array into an int array
+    // i.e. if we have "four". we convert it into a 4 in a new array
     
-    while !isNumber(String(array[ptr1])) {
-        ptr1 += 1
-    }
-    
-    while !isNumber(String(array[ptr2])) {
-        ptr2 -= 1
-    }
+    // Build a new substring and inspect it until it matches one of our valid strings below
+    // What if it doesn't match anything?
+    // I guess
     
     let calibrationValue = Int("\(array[ptr1])\(array[ptr2])")!
     calibrationValueSum += calibrationValue
 }
 
-print(calibrationValueSum)
-
+// Part 2
 func isNumber(_ string: String) -> Bool {
-    let validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    let validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
     return validNumbers.contains(string)
 }
