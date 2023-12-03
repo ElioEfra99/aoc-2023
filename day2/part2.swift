95558f3057099951809d2919e7cab2268c4f3ce9
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2023-12-03 00:16:36 -0600

    Day 2, Part 2 Solution

---
+++ day2/part2.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+let filename = "input"
+let documentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+let fileURL = documentDirURL.appendingPathComponent(filename).appendingPathExtension("txt")
+var fileString = ""
+
+do {
+    fileString = try String(contentsOf: fileURL)
+} catch {
+    fatalError("Failed reading from URL: \(fileURL), Error: \(error.localizedDescription)")
+}
+
+//let games: [[Substring]] = fileString.split(separator: "\n").map { $0.split(separator: ":")[1].split(separator: ";") }
+let games: [[String]] = fileString.split(separator: "\n").map {
+    $0.split(separator: ":")[1].split(separator: ";").map {
+        String($0).trimmingCharacters(in: .whitespaces)
+    }
+} // Probably don't need to clean the string up
+
+let targetGame = ["red": 12, "green": 13, "blue": 14]
+var powerSum = 0
+
+// For each game you need to figure out if it was possible
+for (idx, game) in games.enumerated() {
+    var isGamePossible = true
+    // For every set, build a map, which later we can use to figure out if we
+    var maxColors = ["red": 0, "green": 0, "blue": 0]
+    for `set` in game {
+        // Split every set and build our map
+        let setArray = `set`.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespaces) }
+        typealias GameColor = String
+        var gameMap = [GameColor: Int]()
+        
+        for cube in setArray {
+            let splittedCube = cube.split(separator: " ")
+            let (amount, color) = (Int(splittedCube[0])!, String(splittedCube[1]))
+            
+            if amount > maxColors[color]! {
+                maxColors[color] = amount
+            }
+        }
+        
+    }
+    print("Game \(idx + 1): MaxColors -> \(maxColors)")
+    
+    let (blueAmount, greenAmount, redAmount) = (maxColors["blue"]!, maxColors["green"]!, maxColors["red"]!)
+    powerSum += (blueAmount * greenAmount * redAmount)
+}
+
+print(powerSum)
