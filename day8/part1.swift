a8603625d73456906104b83f592552eff0709a81
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2023-12-09 22:18:03 -0600

    Day 8, Part 1 Solution

---
+++ day8/part1.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+let filename = "input"
+let documentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+let fileURL = documentDirURL.appendingPathComponent(filename).appendingPathExtension("txt")
+var fileString = ""
+
+do {
+    fileString = try String(contentsOf: fileURL)
+} catch {
+    fatalError("Failed reading from URL: \(fileURL), Error: \(error.localizedDescription)")
+}
+
+let splittedInput = fileString.split(separator: "\n\n")
+
+let instructions: [Character] = splittedInput[0].map { $0 }
+let map = splittedInput[1].split(separator: "\n").map { $0.split(separator: "=") }
+var currentNode = "AAA"
+var lastNode = "ZZZ"
+
+var nodes = [String: [String]]()
+
+for node in map {
+    let key = String(node[0]).trimmingCharacters(in: .whitespaces)
+    var networkSegment = String(node[1]).trimmingCharacters(in: .whitespaces)
+    
+    networkSegment.removeLast()
+    networkSegment.removeFirst()
+    
+    let values = networkSegment.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespaces) }
+    
+    nodes[key] = [values[0], values[1]]
+}
+
+var steps = 0
+var instructionsCount = 0
+
+while true {
+    if instructions[instructionsCount] == "L" {
+        currentNode = nodes[currentNode]![0]
+    } else {
+        currentNode = nodes[currentNode]![1]
+    }
+    
+    if instructionsCount == instructions.count - 1 {
+        instructionsCount = 0
+    } else {
+        instructionsCount += 1
+    }
+        
+    steps += 1
+    
+    if currentNode == lastNode {
+        break
+    }
+}
+
+print("Required Steps: \(steps)")
